ef0011c6a72a6a8f712c783ffca25ea09694c865
Craig Erskine <craig@craigerskine.com>
2021-02-24 11:07:52 -0600

    updated syntax to tw v2.x

--- jquery-tailwind-toggle.js
+++ jquery-tailwind-toggle.js
@@ -9,22 +9,21 @@ $(function(){
       var chBoxHandleColor = $(this).data('handle-color');
       chBoxHandleColor = (chBoxHandleColor !== undefined) ? chBoxHandleColor : 'bg-white';
       var chBoxOffColor = $(this).data('off-color');
-      chBoxOffColor = (chBoxOffColor !== undefined) ? chBoxOffColor : 'bg-grey';
+      chBoxOffColor = (chBoxOffColor !== undefined) ? chBoxOffColor : 'bg-gray-500';
       var chBoxOnColor = $(this).data('on-color');
-      chBoxOnColor = (chBoxOnColor !== undefined) ? chBoxOnColor : 'bg-blue-light';
+      chBoxOnColor = (chBoxOnColor !== undefined) ? chBoxOnColor : 'bg-blue-400';
       $(this)
         .attr('data-toggle', 'checkbox-toggle')
-        .css({'width': (chBoxHandleSize * 2.5) + 6 +'px', 'padding': '3px', 'transition': 'all .25s'})
-        .addClass(chBoxRounded +' '+ chBoxOffColor +' inline-flex cursor-pointer align-middle')
-        .append('<b class="'+ chBoxHandleColor +' '+ chBoxRounded +' shadow" style="width: '+ chBoxHandleSize +'px; height: '+ chBoxHandleSize +'px; transition: all .25s" />')
-        .find('input')
-        .addClass('w-px h-px opacity-0 absolute')
-        .attr('tabindex', '-1')
+        .css({'width': (chBoxHandleSize * 2) +'px'})
+        .addClass('inline-flex cursor-pointer align-middle relative')
+        .append('<i class="'+ chBoxRounded +' '+ chBoxOffColor +' absolute inset-x-1 top-1/2 transform -translate-y-1/2 transition duration-200' +'" style="height: '+ (chBoxHandleSize / 2) +'px;" />')
+        .append('<b class="'+ chBoxHandleColor +' '+ chBoxRounded +' relative shadow transition-all duration-200" style="width: '+ chBoxHandleSize +'px; height: '+ chBoxHandleSize +'px;" />')
+        .find('input').addClass('checkbox-toggle-tw w-px h-px opacity-0 absolute').attr('tabindex', '-1')
         .on('change', function(){
           if($(this).is(':checked')){
-            $(this).closest('label').removeClass(chBoxOffColor).addClass(chBoxOnColor).find('b').css('transform', 'translate('+ chBoxHandleSize * 1.5 +'px, 0)');
+            $(this).closest('label').find('i').removeClass(chBoxOffColor).addClass(chBoxOnColor).next('b').css('transform', 'translate('+ chBoxHandleSize * 1 +'px, 0)');
           } else {
-            $(this).closest('label').removeClass(chBoxOnColor).addClass(chBoxOffColor).find('b').css('transform', '');
+            $(this).closest('label').find('i').removeClass(chBoxOnColor).addClass(chBoxOffColor).next('b').css('transform', '');
           }
           if($(this).is(':disabled')){
             $(this).closest('label').addClass('opacity-25 pointer-events-none');
